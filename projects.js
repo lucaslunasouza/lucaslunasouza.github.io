e4c73047d8da53679edd1679f4314d3b05e7477f
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2017-01-23 19:56:35 -0500

    Improved projects screen

--- projects.js
+++ projects.js
@@ -1,9 +1,15 @@
 
 $(document).ready(function () {
-	$("#id1").change(function () {      
-		$("#description").text("test1");
+	$("#id1").change(function () { 
+		$("#p_name").text("MineGame");    
+		$("#p_desc").html("Voxel based game written in Java and Processing.");
 	});
 	$("#id2").change(function () {      
-		$("#description").text("test2");
+		$("#p_name").text("Atlas of Worlds Map");    
+		$("#p_desc").html("A tool for Path of Exile, an online Action RPG.");
 	});
+	$("#id3").change(function () {      
+		$("#p_name").text("Pathfinder");    
+		$("#p_desc").html("Visualization of a pathfinding algorithm for a rectangular grid.");
+	});	
 });
