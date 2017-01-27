984cfd18f3c199cc17a6ee0cc2ddd44b5dab0135
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2017-01-27 05:41:30 -0500

    Updated projects

--- projects.js
+++ projects.js
@@ -1,15 +1,23 @@
 
 $(document).ready(function () {
 	$("#id1").change(function () { 
+		$("#p_name").text("Cordially");    
+		$("#p_desc").html("Calendar web app that allows users to manage their calendar and share it with others.");
+	});
+	$("#id2").change(function () { 
 		$("#p_name").text("MineGame");    
 		$("#p_desc").html("Voxel based game written in Java and Processing.");
 	});
-	$("#id2").change(function () {      
+	$("#id3").change(function () {      
 		$("#p_name").text("Atlas of Worlds Map");    
 		$("#p_desc").html("A tool for Path of Exile, an online Action RPG.");
 	});
-	$("#id3").change(function () {      
+	$("#id4").change(function () {      
 		$("#p_name").text("Pathfinder");    
 		$("#p_desc").html("Visualization of a pathfinding algorithm for a rectangular grid.");
+	});	
+	$("#id5").change(function () {      
+		$("#p_name").text("Lancebotics");    
+		$("#p_desc").html("Built a robot to compete in the VEX Robotics World Championship.");
 	});	
 });
