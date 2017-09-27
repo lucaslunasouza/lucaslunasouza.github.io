2dbe8836f08acc96552226d059307759edbcb264
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2017-09-27 00:18:49 -0400

    Updated Projects

--- projects.js
+++ projects.js
@@ -1,23 +1,31 @@
 
 $(document).ready(function () {
 	$("#id1").change(function () { 
+		$("#p_name").text("Project Solaris");    
+		$("#p_desc").html("Mobile puzzle game where players have shoot projectiles into orbit.");
+	});
+	$("#id2").change(function () { 
+		$("#p_name").text("Request Factory");    
+		$("#p_desc").html("Mobile REST client for sending HTTP requests made for developers who want to test API’s on their phones.");
+	});
+	$("#id3").change(function () {      
 		$("#p_name").text("Cordially");    
 		$("#p_desc").html("Calendar web app that allows users to manage their calendar and share it with others.");
 	});
-	$("#id2").change(function () { 
-		$("#p_name").text("CubeStack");    
-		$("#p_desc").html("Voxel based game written in Java and Processing.");
-	});
-	$("#id3").change(function () {      
-		$("#p_name").text("Atlas of Worlds Map");    
-		$("#p_desc").html("A tool for Path of Exile, an online Action RPG.");
-	});
 	$("#id4").change(function () {      
+		$("#p_name").text("CubeStack");    
+		$("#p_desc").html("Voxel based sandbox game set in a procedurally generated island.");
+	});	
+	$("#id5").change(function () {      
+		$("#p_name").text("Atlas of Worlds Map");    
+		$("#p_desc").html("A tool for tracking progress in Path of Exile, an online Action RPG.");
+	});	
+	$("#id6").change(function () {      
 		$("#p_name").text("Pathfinder");    
 		$("#p_desc").html("Visualization of a pathfinding algorithm for a rectangular grid.");
 	});	
-	$("#id5").change(function () {      
+	$("#id7").change(function () {      
 		$("#p_name").text("Lancebotics");    
-		$("#p_desc").html("Built a robot to compete in the VEX Robotics World Championship.");
+		$("#p_desc").html("Built a robot to compete in the 2015 VEX Robotics World Championship.");
 	});	
 });
