ef8669cab6353f9f34c1dcb706515e25c683c328
Lucas Souza <lucas.lunasouza@mail.utoronto.ca>
2018-01-23 11:25:30 -0500

    Added some projects

--- projects.js
+++ projects.js
@@ -1,30 +1,42 @@
 
 $(document).ready(function () {
 	$("#id1").change(function () { 
+		$("#project-name").text("Glance");    
+		$("#project-desc").html("Interactive map of world news.");
+	});
+	$("#id2").change(function () { 
+		$("#project-name").text("Showgram");    
+		$("#project-desc").html("Social media platform for discussing TV show episodes.");
+	});
+	$("#id3").change(function () { 
 		$("#project-name").text("Project Solaris");    
 		$("#project-desc").html("Mobile puzzle game where players have shoot projectiles into orbit.");
 	});
-	$("#id2").change(function () { 
+	$("#id4").change(function () { 
+		$("#project-name").text("Spectrum Sorter");    
+		$("#project-desc").html("A way to visualize sorting algorithms by using a colour spectrum.");
+	});
+	$("#id5").change(function () { 
 		$("#project-name").text("Request Factory");    
 		$("#project-desc").html("Mobile REST client for sending HTTP requests made for developers who want to test API’s on their phones.");
 	});
-	$("#id3").change(function () {      
+	$("#id6").change(function () {      
 		$("#project-name").text("Cordially");    
 		$("#project-desc").html("Calendar web app that allows users to manage their calendar and share it with others.");
 	});
-	$("#id4").change(function () {      
+	$("#id7").change(function () {      
 		$("#project-name").text("CubeStack");    
 		$("#project-desc").html("Voxel based sandbox game set in a procedurally generated island.");
 	});	
-	$("#id5").change(function () {      
+	$("#id8").change(function () {      
 		$("#project-name").text("Atlas of Worlds Map");    
 		$("#project-desc").html("A tool for tracking progress in Path of Exile, an online Action RPG.");
 	});	
-	$("#id6").change(function () {      
+	$("#id9").change(function () {      
 		$("#project-name").text("Pathfinder");    
 		$("#project-desc").html("Visualization of a pathfinding algorithm for a rectangular grid.");
 	});	
-	$("#id7").change(function () {      
+	$("#id10").change(function () {      
 		$("#project-name").text("Lancebotics");    
 		$("#project-desc").html("Built a robot to compete in the 2015 VEX Robotics World Championship.");
 	});	
