85f640bf79968744c41047d114b9f3b30306ea54
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2019-08-29 00:07:42 -0400

    Improved project grid on mobile

--- src/components/ProjectList/style.js
+++ src/components/ProjectList/style.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import {
-  TABLET_BREAKPOINT
+  DESKTOP_BREAKPOINT,
+  TABLET_BREAKPOINT,
+  MOBILE_BREAKPOINT
 } from '../../shared/styles/layout'
 import theme from '../../shared/styles/theme'
 
@@ -22,16 +24,25 @@ export const ProjectListTitle = styled.div`
 
 export const ProjectsGrid = styled.div`
   height: 65vh;
-  width: 80vw;
+  width: 60vw;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(2, 1fr);
   grid-row-gap: 50px;
   grid-column-gap: 30px;
+  @media only screen and (max-width: ${DESKTOP_BREAKPOINT}) {
+    width: 70vw;
+  }
   @media only screen and (max-width: ${TABLET_BREAKPOINT}) {
     grid-template-columns: repeat(2, 1fr);
     height: 55vh;
+    width: 70vw;
+    grid-row-gap: 30px;
+  }
+  @media only screen and (max-width: ${MOBILE_BREAKPOINT}) {
     width: 90vw;
+    grid-template-columns: repeat(2, 1fr);
+    height: 55vh;
     grid-row-gap: 30px;
   }
 `
