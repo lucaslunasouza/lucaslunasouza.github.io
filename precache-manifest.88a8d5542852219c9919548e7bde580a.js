5cb03745543d75b9112349091145d6b5d58a6821
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2019-08-28 23:53:32 -0400

    Updates

---
+++ precache-manifest.88a8d5542852219c9919548e7bde580a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d8906882a036d433f65edaf452b4c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b981af476d43e66a17df",
+    "url": "/static/css/main.c4e3a9a2.chunk.css"
+  },
+  {
+    "revision": "0a49da5748c134f45ea1",
+    "url": "/static/js/2.aabf0028.chunk.js"
+  },
+  {
+    "revision": "b981af476d43e66a17df",
+    "url": "/static/js/main.4ed3a467.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6818b2ab5079ec026294fbb903c34c21",
+    "url": "/static/media/lucas-resume.6818b2ab.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lnsz.ca/precache-manifest.ffe63b2e67056b509ba9970c32d0d477.js"
+  "/precache-manifest.88a8d5542852219c9919548e7bde580a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lnsz.ca/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
