960aa6ae5504ee4d29ef0f2f84af0631cc552805
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2019-08-28 23:19:19 -0400

    Updates

---
+++ precache-manifest.ffe63b2e67056b509ba9970c32d0d477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184b5dd31b0a958c1251c7a9520e834c",
+    "url": "/lnsz.ca/index.html"
+  },
+  {
+    "revision": "1c3b1d698bc85be3e414",
+    "url": "/lnsz.ca/static/css/main.c4e3a9a2.chunk.css"
+  },
+  {
+    "revision": "f74bb1dd660bf1d0f6bd",
+    "url": "/lnsz.ca/static/js/2.33b152e5.chunk.js"
+  },
+  {
+    "revision": "1c3b1d698bc85be3e414",
+    "url": "/lnsz.ca/static/js/main.b19375e8.chunk.js"
+  },
+  {
+    "revision": "5b3e4a85753f51c2947f",
+    "url": "/lnsz.ca/static/js/runtime~main.db5d9a60.js"
+  },
+  {
+    "revision": "6818b2ab5079ec026294fbb903c34c21",
+    "url": "/lnsz.ca/static/media/lucas-resume.6818b2ab.pdf"
+  }
+]);
